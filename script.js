1e5d58112d8e7d36be92236b2bda8d9cd7782777
Muhtasim Mushfiq <muhtasim01@gmail.com>
2024-11-12 10:32:02 -0500

    Implemented mailto feature in contact form section

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x')
     navbar.classList.toggle('active')
 }
+
+function sendMail(){
+    var body = document.getElementById('message').value;
+    var subject = document.getElementById('subject').value;
+    var name = document.getElementById('name').value;
+    var number = document.getElementById('number').value;
+    var email = document.getElementById('email').value;
+
+    window.location.href = "mailto:muhtasim01@gmail.com"
+    + "?from=" + encodeURIComponent(email)
+    + "&subject=" + encodeURIComponent(subject)
+    + "&body=" 
+    + encodeURIComponent("Email: " + email + "\nName: " + name + "\nPhone Number: " + number + "\n\n" + body);
+}
